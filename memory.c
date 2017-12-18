7a1830caba46f93b0e2a6a6961768e98aca776f6
Tim Martin <martint@f203-12.e-im2ag.ujf-grenoble.fr>
2017-12-18 17:29:41 +0100

    nique seg fault

--- memory.c
+++ memory.c
@@ -1,64 +1,127 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include <stdlib.h>
 #include "memory.h"
 #include "util.h"
 
 struct memory_data {
+  uint32_t *memoire;
+  size_t size;
+  int is_big_endian;
 };
 
 memory memory_create(size_t size, int is_big_endian) {
-    memory mem=NULL;
-    return mem;
+    memory mem= malloc(sizeof(memory));
+    if(mem){
+      mem->memoire = malloc(size*sizeof(uint32_t));
+      mem->size = size;
+      mem->is_big_endian = is_big_endian;
+      return mem;
+    }else{
+      exit;
+    }
 }
 
 size_t memory_get_size(memory mem) {
-    return 0;
+  return mem->size;
 }
 
 void memory_destroy(memory mem) {
+  free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+    if(mem->memoire + address <=  ( mem->memoire + mem->size - sizeof(uint8_t) ) ) {
+        uint8_t * result = mem->memoire + address;
+        value = result;
+        return 0;
+    }else{
+      return -1;
+    }
 }
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value) {
+  if(mem->memoire + address <=  ( mem->memoire + mem->size - sizeof(uint16_t) ) ) {
+    uint16_t * result;
+    if( mem->is_big_endian){
+      result = mem->memoire + address;
+    }else{
+      result = ((uint16_t)(mem->memoire + address)<<8) | ((uint16_t)(mem->memoire + address)>>8);
+    }
+      value = result;
+      return 0;
+  }else{
     return -1;
+  }
+
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
+  if(mem->memoire + address <=  ( mem->memoire + mem->size - sizeof(uint32_t) ) ) {
+      uint32_t * result;
+    if( mem->is_big_endian){
+      result = mem->memoire + address;
+    }else{
+      uint32_t masque = 0x000000FF;
+      uint32_t * tmp = mem->memoire + address;
+     result= (masque & (uint32_t)tmp>>24)
+      + ((masque<<8) & (uint32_t)tmp>>8)
+      + ((masque<<8) & (uint32_t)tmp<<8)
+      + ((masque<<8) & (uint32_t)tmp<< 24);
+    }
+      value = result;
+      return 0;
+  }else{
     return -1;
+  }
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
+  if(mem->memoire + address <=  ( mem->memoire + mem->size - sizeof(uint8_t) ) ) {
+      uint8_t * result = mem->memoire + address;
+      result = value;
+      return 0;
+  }else{
     return -1;
+  }
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value) {
+  if(mem->memoire + address <=  ( mem->memoire + mem->size - sizeof(uint16_t) ) ) {
+      uint16_t * result = mem->memoire + address;
+      result = value;
+      return 0;
+  }else{
     return -1;
+  }
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value) {
+  if(mem->memoire + address <=  ( mem->memoire + mem->size - sizeof(uint32_t) ) ) {
+      uint32_t * result = mem->memoire + address;
+      result = value;
+      return 0;
+  }else{
     return -1;
+  }
 }
--- registers.c
+++ registers.c
@@ -1,80 +1,107 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "registers.h"
 #include "arm_constants.h"
 #include <stdlib.h>
 
 struct registers_data {
+  uint32_t* R;
 };
 
 registers registers_create() {
-    registers r = NULL;
+    registers r = malloc(sizeof(registers));
+    r->R = malloc(37*sizeof(uint32_t));
     return r;
 }
 
 void registers_destroy(registers r) {
+  free(r);
 }
 
+/*
+ 16: mode User
+ 17: mode FIQ
+ 18: mode IRQ
+ 19: mode Supervisor
+ 23: mode Abort
+ 27: mode Undefined
+ 31: mode System
+*/
+
 uint8_t get_mode(registers r) {
-    return 0;
-} 
+  uint8_t masque = 0x1F;
+  return (masque & r->R[16]);
+}
 
 int current_mode_has_spsr(registers r) {
-    return 0;
+  uint8_t mode = get_mode(r);
+  return (mode != 16 && mode != 31);
 }
 
 int in_a_privileged_mode(registers r) {
-    return 0;
+  uint8_t mode = get_mode(r);
+  return (mode != 16);
 }
 
 uint32_t read_register(registers r, uint8_t reg) {
-    uint32_t value=0;
-    return value;
+    return r->R[reg];
 }
 
 uint32_t read_usr_register(registers r, uint8_t reg) {
     uint32_t value=0;
-    return value;
+    if(! in_a_privileged_mode(reg)){
+      return r->R[reg];
+    }else{
+      exit;
+    }
 }
 
 uint32_t read_cpsr(registers r) {
-    uint32_t value=0;
-    return value;
+    return r->R[16];
 }
 
 uint32_t read_spsr(registers r) {
-    uint32_t value=0;
-    return value;
+  if(current_mode_has_spsr(r)){
+      return r->R[17];
+  }
 }
 
 void write_register(registers r, uint8_t reg, uint32_t value) {
+  r->R[reg] = value;
 }
 
 void write_usr_register(registers r, uint8_t reg, uint32_t value) {
+  if(! in_a_privileged_mode(reg)){
+    r->R[reg] = value;
+  }else{
+    exit;
+  }
 }
 
 void write_cpsr(registers r, uint32_t value) {
+  r->R[16] = value;
 }
 
 void write_spsr(registers r, uint32_t value) {
+    r->R[17] = value;
 }
