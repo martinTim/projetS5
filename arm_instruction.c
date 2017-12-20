cb05c4210c167814210f07571b71b4da1d34ad41
Tim Martin <martint@F202-06.e-im2ag.ujf-grenoble.fr>
2017-12-20 10:32:53 +0100

    fixe du condCode

--- arm_instruction.c
+++ arm_instruction.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -39,4 +39,30 @@ int arm_step(arm_core p) {
     if (result)
         arm_exception(p, result);
     return result;
+}
+
+int condCode(arm_core p,uint32_t value){
+
+    uint8_t opcode = (0xF0000000 & value)>>28;
+    uint8_t NZCV = arm_read_cpsr(p)>>28;
+    switch (opcode){
+      case 0: return (NZCV & 0b0100); break;  // Z set
+      case 1: return !(NZCV & 0b0100); break; // Z clear
+      case 2: return (NZCV & 0b0010); break;  // C set
+      case 3: return !(NZCV & 0b0010); break; // C clear
+      case 4: return (NZCV & 0b1000); break; // N set
+      case 5: return !(NZCV & 0b1000); break; // N clear
+      case 6: return (NZCV & 0b0001); break; // V set
+      case 7: return !(NZCV & 0b001); break; // V clear
+      case 8: return ((NZCV & 0b0010) && !(NZCV & 0b0100)); break; // C set Z clear
+      case 9: return (!(NZCV & 0b0010) || (NZCV & 0b0100)); break; // C clear or Z set
+      case 10:return (((NZCV & 0b1000)>>3) == (NZCV & 0b0001)) ; break; // N == V
+      case 11:return ((NZCV & 0b1000)^(NZCV & 0b0001)); break; // N != V
+      case 12:return ((((NZCV & 0b1000)>>3) == (NZCV & 0b0001)) && !(NZCV & 0b0100)); break; // Z clear and N == V
+      case 13:return ((NZCV & 0b0100) || (((NZCV & 0b1000)>>3) == (NZCV & 0b0001))); break;
+      case 14:return 1; break; // toujours vrai
+      case 15:return 0; break; // aucune idÃ©e
+      default: return -1; break;
+    }
+
 }
