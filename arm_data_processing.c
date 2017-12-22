0d10d4e2b20146bfd5b8dd38893a99c37f3e5c08
Sophie Romano <romanoso@F202-04.e-im2ag.ujf-grenoble.fr>
2017-12-22 11:25:34 +0100

    opeerations

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -89,4 +89,39 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
 
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
+}
+
+int operation(uint32_t rd*,uint8_t operande,  uint32_t rn, uint32_t shifter_operande){
+	switch(operande){
+		case 0: //AND  (Rd := Rn AND shifter_operande)
+		case 8: //TST
+			*rd := (rn & shifter_operande);
+			break;
+		case 1: //EOR  (Rd := Rn XOR shifter_operande) 
+		case 9: //TEQ
+			break;
+		case 2: //SUB  (Rd := Rn - shifter_operande) 
+		case 10://CMP
+			break;
+		case 3: //RSB reverse substract
+			break;
+		case 4: //ADD
+		case 11://CMN
+			break;
+		case 5: // ADC add with carry
+			break;
+		case 6: // SBC sub with carry
+			break;
+		case 7: // RSC reverse substract with carry
+			break;
+		case 12: // ORR logical OR
+			break;
+		case 13: // MOV
+			break;
+		case 14: //BIC (rd = rn AND NOT shifter_operand)
+			break;
+		case 15: //MNV ( rd != NOT shifter_operand)
+			break;
+	}
+	
 }
