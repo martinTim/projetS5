c23b849ac54ae8437ff02c1abd2f35795b045628
Maxence Bleton--Giordano <bletongm@F202-03.e-im2ag.ujf-grenoble.fr>
2017-12-22 10:27:58 +0100

    data processing

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -30,63 +30,79 @@ Contact: Guillaume.Huard@imag.fr
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
 	
+	
 	uint8_t opcode = get_bits(ins,24,21);
 	uint8_t S = get_bit(ins,20);
+
+
+	//first operand
 	uint8_t Rn = get_bits(ins,19,16);
-	uint32_t rn_val = p->arm_read_register(p,Rn);	
-	uint8_t Rd = get_bits(ins,15,12);
+	uint32_t rn_val = arm_read_register(p,Rn);	
+	
+	//registre de  destination
+	uint8_t Register_destination = get_bits(ins,15,12);
 	uint32_t rd_val;
-	uint8_t S = get_bit(ins,25);
+
+	//result of function
 	int res;
 	
-	switch(opcode){
-		case ADD:
-			uint8_t shift_type = get_bits(ins,6,5);
-			if(get_bit(ins,4)){ // register 
-				uint8_t reg = get_bits(ins,3,0);
-				uint32_t rm  = p->arm_read_register(p,reg);		
-				uint32_t val_shift = p->arm_read_register(p,get_bit(ins,11,8));			
-				res = shift(rm,shift_type,val_shift);
-				if(!res){
-					rd_val = rn_val + Rm;
-					p->arm_write_register(p,Rd,rd_val);
-				}else{
-					return -1;
-				}
-			}else{//immediat
-			
-				uint8_t val_shift = get_bits(ins,3,0);
-			
-			
-				if(!res){
-					rd_val = rn_val + Rm;
-					p->arm_write_register(p,Rd,rd_val);
-				}else{
-					return -1;
-				}			
-			}
-			
+
+	if(get_bit(ins,7) && get_bit(ins,4))//instruction is not a ADD
+		return -1;
+
+
+	//type du shift
+	uint8_t shift_type = get_bits(ins,6,5);
+
+	uint32_t value_To_Shift;
+	uint32_t val_shift;
+	uint8_t register_decalage = get_bits(ins,3,0);
+	if(get_bit(ins,4)){ // decalage par registre 
+
 		
-		break;
-		
+		value_To_Shift  = arm_read_register(p,register_decalage);		
+	    val_shift = arm_read_register(p,get_bits(ins,11,8));		
+
+	}else{// decalage immediat 
+
+		uint8_t register_decalage = get_bits(ins,3,0);
+		value_To_Shift  = arm_read_register(p,register_decalage);	
+		val_shift = arm_read_register(p,get_bits(ins,11,7));
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+	res = shift(value_To_Shift,shift_type,val_shift);
+
+	//traiter carry shift
+
+
+	res = operation(opcode,rd_val,rn_val,value_To_Shift);// (opcode,dest,fisrtOP,secOp)
+
+
+	rd_val =  arm_read_register(p,Register_destination);	
+
+
+	if(S == 1 && rd_val == arm_read_register(p,15)){
+		if( arm_current_mode_has_spsr){//=> CPSR = SPSR
+			arm_write_register(p,16,arm_read_register(p,17));	
+		}else{
+			res = -1;
+		}
+
+
+	}else if(S == 1){//mise a jours des registres ZNVC
+
+
+	}
 
 	
-	
-    return UNDEFINED_INSTRUCTION;
+
+    return res;
 }
 
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
 }
+
+
