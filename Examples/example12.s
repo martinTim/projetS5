8e1a17092c563abc313469fe7d99bd5d9414e346
geoliak <g.liakopoulos@hotmail.com>
2018-01-12 13:07:42 +0100

    dernier test

--- Examples/example12.s
+++ Examples/example12.s
@@ -2,12 +2,15 @@
 .text
 
 main:
-    mov r0, #0xFFFFFFFF
-    add r1,r1,r0
-	
-
-
-
+	mov r0, #0x00000010
+    mov r1, #0xFFFFFFFF
+    mov r2, #0xFFFFFF0F
+    cmp r0, #0x00000011
+    bcc main
+    tst r2,r0
+    adds r0,r0,r0
+    sbc r3,r0,r0 
+    rscs r4, r2, r1
 fin:
 	swi 0x123456
 .data
